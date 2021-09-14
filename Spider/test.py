cc41f06fa1e5187befb947cfd8d45994045946aa
weixiabing <weixiabing@hotmail.com>
2021-09-14 19:02:18 +0800

    1

--- Spider/test.py
+++ Spider/test.py
@@ -18,6 +18,7 @@ options.add_argument('--no-sandbox')
 # 无头参数
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
+options.add_argument('--user-agent=Mozilla/5.0 (Linux; Android 11; Redmi K30 Pro Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045811 Mobile Safari/537.36 MMWEBID/8641 MicroMessenger/8.0.11.1980(0x28000B51) Process/tools WeChat/arm64 Weixin NetType/5G Language/zh_CN ABI/arm64')
 # 启动浏览器
 browser = Chrome(executable_path=DRIVER_PATH, options=options)
 # 访问url
