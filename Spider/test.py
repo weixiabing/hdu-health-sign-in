bacde49956a9a6620c526664fb77cdfe08f370d1
weixiabing <weixiabing@hotmail.com>
2021-09-15 22:04:31 +0800

    1

--- Spider/test.py
+++ Spider/test.py
@@ -6,8 +6,8 @@ DRIVER_PATH = '/usr/bin/chromedriver'
 
 options = Options()
 options.add_argument('--no-sandbox')
-    #options.add_argument('--headless')  # 无头参数
-    #options.add_argument('--disable-gpu')
+options.add_argument('--headless')  # 无头参数
+options.add_argument('--disable-gpu')
     #换成微信浏览器ua
 options.add_argument('--user-agent=Mozilla/5.0 (Linux; Android 11; Redmi K30 Pro Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045811 Mobile Safari/537.36 MMWEBID/8641 MicroMessenger/8.0.11.1980(0x28000B51) Process/tools WeChat/arm64 Weixin NetType/5G Language/zh_CN ABI/arm64')
 def main(username,password):
