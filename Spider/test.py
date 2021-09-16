bd95ee7a9cfb7d4fb2b82d330d149371607f189e
Wei.Xiabing <weixiabing@hotmail.com>
2021-09-16 15:48:12 +0800

    d

--- Spider/test.py
+++ Spider/test.py
@@ -99,7 +99,7 @@ def main(username,password):
     #tg 推送功能
 if __name__ == "__main__":
     print(sys.argv)
-    '''username = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
     
     
@@ -108,4 +108,4 @@ if __name__ == "__main__":
         post_tg('恭喜'+username[i]+'打卡成功')
     except:
         print('检查账号密码或脚本已失效或您已达过卡')
-        post_tg('检查账号密码或脚本已失效或您已达过卡')'''
+        post_tg('检查账号密码或脚本已失效或您已达过卡')
